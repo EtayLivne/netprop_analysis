eb0a2d088cf22a335826eae60eccbb261de58961
Etay <etay.livne@gmail.com>
2022-03-26 12:07:12 +0300

    "it's leviosaaaaaaaa"

---
+++ metrics/metric.py
@@ -0,0 +1,48 @@
+
+from multiprocessing import Pool, cpu_count
+import pandas as pd
+from abc import abstractmethod, ABC
+from netprop.models import PropagationResultModel
+
+
+class MetricRuntimeError(Exception):
+    pass
+
+
+class Metric(ABC):
+    @abstractmethod
+    def calc_metric(self):
+        raise NotImplemented
+
+
+class SinglePropMetric(Metric):
+    _GENE_COLUMN_NAME = "genes"
+    _SCORE_COLUMN_NAME = "scores"
+
+    def __init__(self, prop_file_path=None):
+        if prop_file_path is None:
+            self._prop_df = pd.DataFrame(columns=[self._GENE_COLUMN_NAME, self._SCORE_COLUMN_NAME])
+        else:
+            self.prop_data_to_df(prop_file_path)
+
+    def prop_data_to_df(self, prop_file_path: str):
+        res = PropagationResultModel.parse_file(prop_file_path)
+        self._prop_data_to_df(res)
+
+    @abstractmethod
+    def _prop_data_to_df(self, res: PropagationResultModel):
+        self._prop_df = None
+
+
+class MultiPropMetric(Metric):
+    def __init__(self):
+        self._props_df = None
+
+    def props_data_to_df(self, prop_file_paths: list[str], max_proc=min(cpu_count(), 40)):
+        with Pool(min(len(prop_file_paths), max_proc)) as p:
+            res_list = p.map(PropagationResultModel.parse_file, prop_file_paths)
+        self._prop_data_to_df(res_list)
+
+    @abstractmethod
+    def _prop_data_to_df(self, res_list: list[PropagationResultModel]):
+        self._props_df = None
---
+++ metrics/roc_auc.py
@@ -0,0 +1,42 @@
+from metrics.metric import SinglePropMetric, MetricRuntimeError
+import pandas as pd
+import matplotlib.pyplot as plt
+from crispr import get_huh_crispr
+
+class SinglePropROC(SinglePropMetric):
+
+    def __init__(self, prop_file_path=None):
+        super().__init__(prop_file_path=prop_file_path)
+        self._hit_set_load_method = None
+        self._hit_set = None
+
+    def load_hit_set(self, data_file_path: str):
+        self._hit_set = self._hit_set_load_method(data_file_path)
+
+
+    def measure_roc(self, print_plot=True):
+        if not self._hit_set:
+            raise MetricRuntimeError("cannot measure roc before hit set is defined!")
+        elif not self._prop_df:
+            raise MetricRuntimeError("cannot measure roc before prop results are defined!")
+
+        hit_df = self._prop_df[self._GENE_COLUMN_NAME].isin(self._hit_set)
+        positives = hit_df.sum()
+        negatives = len(hit_df) - positives
+        tpr = hit_df.cumsum() / positives
+        fpr = ~hit_df.cumsum() / negatives
+
+        # self._plot_roc(roc_df := pd.concat([tpr, fpr], axis=1))
+        return pd.concat([tpr, fpr], axis=1)
+
+    #TODOD
+    # def _plot_roc(self, roc_df):
+    #     plt.plot()
+
+
+class HuhCrisprROC(SinglePropROC):
+    def __init__(self, prop_file_path=None, data_file_path=None):
+        super().__init__(prop_file_path=prop_file_path)
+        self._hit_set_load_method = get_huh_crispr
+        if data_file_path is not None:
+            self.load_hit_set(data_file_path)
