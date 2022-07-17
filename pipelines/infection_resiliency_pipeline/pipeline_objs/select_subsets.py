25a4813715ed7549ead386d0a5dc4cb72289715f
Etay <etay.livne@gmail.com>
2022-07-17 13:13:11 +0300

    continued draft of new pipeline

--- pipelines/infection_resiliency_pipeline/pipeline_objs/select_subsets.py
+++ pipelines/infection_resiliency_pipeline/pipeline_objs/select_subsets.py
@@ -5,8 +5,9 @@ from utils.utils import dump_json, load_json
 from pathlib import Path
 import pandas as pd
 import numpy as np
-from multiprocessing import Process
-
+from ray import remote
+from ray.util.queue import Queue
+from multiprocessing import cpu_count
 def _num_subsets(set_size: int, subset_ration: float) -> int:
     return 5    #TODO actual math based calculation
 
@@ -26,19 +27,36 @@ def subsets_file(sets: dict[str, list[str]], subset_ratios: list[float], output_
         results[set_name] = {ratio: list(_gen_subsets(set_members, ratio)) for ratio in subset_ratios}
     dump_json(results, output_path)
 
+@remote
+class subset_ranker:
+    def __init__(self, full_res_path: str):
+        self.df = pd.read_csv(full_res_path)
 
-def _single_df
+    def specific_subset_ranking(self, subset: list[str]) -> pd.Series:
+        self.df["subset"] = self.df.apply(lambda row: row[subset].sum(), axis=1)
+        self.df = self.df.sort_values("subset", ascending=False)
+        self.df["ranks"] = pd.Series(np.arange(len(self.df)))
+        return self.df["ranks"]
 
-
-def rank_by_subset_df(subsets_file: str, prop_res_file: str, output_dir: str) -> None:
+def rank_by_subset_df(subsets_file: str, prop_res_file: str, output_dir: str, num_workers=2*(cpu_count() - 2)) -> None:
     subsets = load_json(subsets_file)
     prop_df = pd.read_csv(prop_res_file)
     output_dir_path = Path(output_dir)
+    actors = [subset_ranker.remote(prop_res_file) for _ in range(num_workers)]
+    q = Queue()
+
     for set_name, subset_ratios in subsets.items():
         set_output_dir = output_dir_path / set_name
         set_output_dir.mkdir()
         for subset_ratio, subsets in subset_ratios.items():
             set_ratio_df = pd.DataFrame(index=prop_df.index)
+            subset_list = list(enumerate(subsets))
+            rank_series_list = []
+            # nope just do with pool
+            while subset_list:
+                slice_size = min(len(subset_list), len(rank_series_list))
+                rank_series_list.extend([actors[i].specific_subset_ranking.remote(subset_list[i]) for i in range(slice_size)])
+                subset_list = subset_list[slice_size:]
             for i, subset in enumerate(subsets):
                 prop_df["subset"] = prop_df.apply(lambda row: row[subset].sum(), axis=1)
                 prop_df = prop_df.sort_values("subset", ascending=False)
